d49516c42564ddf4b005cb2d5f7ea0a18e6140dc
Meaghan Winter <mewinter@uvm.edu>
2015-10-21 10:22:11 -0400

    friday assignment work

--- misc/friday.php
+++ misc/friday.php
@@ -8,45 +8,54 @@
 
 include "top.php";
 
-if(isset($_GET['start'])){
-        $start= (int) $_GET ["start"];
+if (isset($_GET['start'])) {
+    $start = (int) $_GET ["start"];
+}else{
+    $start = 0;
 }
-//now print out each record
-$columns = 8;
-$query = "SELECT * FROM tblStudents ORDER BY fldLastName, fldFirstName LIMIT 10, " . $start;
+
+$query = "SELECT * FROM tblStudents ORDER BY fldLastName, fldFirstName LIMIT 10 OFFSET ".$start;
 
 $info2 = $thisDatabaseReader->select($query, "", 0, 1, 0, 0, false, false);
-// $info2 = $thisDatabaseReader->testquery($query, "", 0, 1, 0, 0, false, false);
+//$info2 = $thisDatabaseReader->testquery($query, "", 0, 1, 0, 0, false, false);
+
 
 $highlight = 0; // used to highlight alternate rows
-$fields = array_keys($info[0]);
+
+print '<article><p><b>Total Records: ' . count($info2) . '</b></p>';
+print '<p><b>SQL: ' . $query . '</b></p>';
+
+// the array $records is both associative and indexed, column zero is associative
+// which you see in teh above print_r statement
+$fields = array_keys($info2[0]);
 $labels = array_filter($fields, "is_string");
 
-$camelCase = preg_split('/(?=[A-Z])/', substr($key, 3));
 
-foreach ($camelCase as $one) {
-    $message .= $one . " ";
-}
-
-print "<p><pre>";
-print_r ($fields);
-print '<p><b>Total Records: ' . count($info2) . '</b></p>';
-print '<p><b>SQL: ' . $query . '</b></p>';
-print '<table>';
-foreach ($info2 as $rec) {
-    $highlight++;
-    if ($highlight % 2 != 0) {
-        $style = ' odd ';
-    } else {
-        $style = ' even ';
+$columns = count($labels);
+print '<table><article>';
+print '<tr><th colspan="' . $columns . '">' . $query . '</th></tr>';
+// print out the column headings, note i always use a 3 letter prefix
+// and camel case like pmkCustomerId and fldFirstName
+print '<tr>';
+foreach ($labels as $label) {
+    print '<th>';
+    $camelCase = preg_split('/(?=[A-Z])/', substr($label, 3));
+    foreach ($camelCase as $one) {
+        print $one . " ";
     }
-    print '<tr class="' . $style . '">';
+    print '</th>';
+}
+print '</tr>';
+//now print out each record
+foreach ($info2 as $record) {
+    print '<tr>';
     for ($i = 0; $i < $columns; $i++) {
-        print '<td>' . $rec[$i] . '</td>';
+        print '<td>'. $record[$i] . '</td>';
     }
     print '</tr>';
 }
 // all done
-print '</table>';
+print '</table></article>';
+
 include "footer.php";
 ?>
